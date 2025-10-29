72119678a5e42dd1a7bff73277bb47d81a728d73
p.bukharov <p.bukharov@rendez-vous.ru>
2025-10-29 18:21:33 +0300

    upd

--- accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
@@ -1,23 +1,17 @@
 package com.microservices.accounts;
 
-import com.microservices.accounts.dto.CustomerDto;
-import com.microservices.accounts.service.AccountsService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
-@OpenAPIDefinition(info =
-@Info(title = "Accounts API", version = "1.0", description = "Accounts API"))
+@OpenAPIDefinition(info = @Info(title = "Accounts API", version = "1.0", description = "Accounts API"))
 public class AccountsApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
     }
 
